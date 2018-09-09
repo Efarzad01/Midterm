e5e6b020a72e7163d717fd2c19abde9597842dde
Efarzad01 <emdadullahfarzad@gmail.com>
2018-09-09 16:18:00 -0400

    Pyramid

--- src/math/problems/MakePyramid.java
+++ src/math/problems/MakePyramid.java
@@ -14,6 +14,17 @@ public class MakePyramid {
 
         */
 
-
+            for (int i = 0; i < 9; i++) {
+                for (int j = 0; j < 9 - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int k = 0; k <= i; k++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
+            }
         }
-  }
+    }
+
+
+
