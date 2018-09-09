c34493204d3553703d94dfac80cb5b7b7081ece5
Efarzad01 <emdadullahfarzad@gmail.com>
2018-09-09 17:43:05 -0400

    Map

--- src/datastructure/UseArrayList.java
+++ src/datastructure/UseArrayList.java
@@ -1,5 +1,7 @@
 package datastructure;
 
+import java.util.*;
+
 public class UseArrayList {
 
 	public static void main(String[] args) {
@@ -7,10 +9,34 @@ public class UseArrayList {
 		 * Demonstrate how to use ArrayList that includes add,peek,remove,retrieve elements.
 		 * Use For Each loop and while loop with Iterator to retrieve data.
 		 * Store all the sorted data into one of the databases.
-		 * 
+		 *
 		 */
-	
+		List<String> CityUsa = new ArrayList<String>();
 
+		CityUsa.add("NY");
+		CityUsa.add("NJ");
+		CityUsa.add("CA");
+
+		List<String> CityBD = new ArrayList<String>();
+
+		CityBD.add("DHAKA");
+		CityBD.add("CTG");
+		CityBD.add("SLT");
+
+		Map<String, List<String>> ListOfCon = new HashMap<String,List<String>>();
+
+		ListOfCon.put("USA", CityUsa);
+		ListOfCon.put("BD", CityBD);
+
+		for (Map.Entry<String, List<String>> state:ListOfCon.entrySet()){
+
+			if (ListOfCon.get("USA").contains("NY")){
+				System.out.println(state.getKey()+"----->" + state.getValue());
+
+			}
+
+		}
+
+
+		}
 	}
-
-}
--- src/datastructure/UseMap.java
+++ src/datastructure/UseMap.java
@@ -1,5 +1,8 @@
 package datastructure;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UseMap {
 
 	public static void main(String[] args) {
@@ -10,7 +13,17 @@ public class UseMap {
 		 *
 		 * Use any databases[MongoDB, Oracle, MySql] to store data and retrieve data.
 		 */
+
               //for each
+		Map<String,String > CountryNCity = new HashMap<String, String>();
+
+		CountryNCity.put("USA","NY");
+		CountryNCity.put("CANADA","MONTREAL");
+		CountryNCity.put("BANGLADESH", "DHAKA");
+
+		for(Map.Entry<String,String> state:CountryNCity.entrySet()){
+			System.out.println(state.getKey()+ "----->" + state.getValue());
+		}
 	}
 
 }
