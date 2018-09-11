e996f5cf818159ceb74a957e5415915c05ac0535
Efarzad01 <emdadullahfarzad@gmail.com>
2018-09-10 23:51:30 -0400

    core java ans

--- src/string/problems/UnitTestingStringProblem.java
+++ src/string/problems/UnitTestingStringProblem.java
@@ -1,8 +1,15 @@
 package string.problems;
 
+import org.testng.Assert;
+
 public class UnitTestingStringProblem {
     public static void main(String[] args) {
         //Apply Unit Test into all the methods in this package.
+        Assert.assertEquals(true,Palindrome.checkPalindrome("DAD")
 
+
+        String ="The cat jumped over the head of the sly fox";
+        Assert.assertEquals("jumped",DetermineLargestWord.findTheLargestWord().largestWord(s));
+        Assert.assertEquals(false,Anagram.isAnagram("TON","TAN"));
     }
 }
