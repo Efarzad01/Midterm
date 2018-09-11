734c764ebc9f37cf26c0d5bef892782ad7384352
Efarzad01 <emdadullahfarzad@gmail.com>
2018-09-11 09:16:20 -0400

    Unit test

--- src/string/problems/UnitTestingStringProblem.java
+++ src/string/problems/UnitTestingStringProblem.java
@@ -5,11 +5,9 @@ import org.testng.Assert;
 public class UnitTestingStringProblem {
     public static void main(String[] args) {
         //Apply Unit Test into all the methods in this package.
-        Assert.assertEquals(true,Palindrome.checkPalindrome("DAD")
-
-
-        String ="The cat jumped over the head of the sly fox";
-        Assert.assertEquals("jumped",DetermineLargestWord.findTheLargestWord().largestWord(s));
-        Assert.assertEquals(false,Anagram.isAnagram("TON","TAN"));
+        Assert.assertEquals(true, Palindrome.checkPalindrome("DAD"));
+        String s="It is forbidden to proceed inside";
+        Assert.assertEquals("forbidden", DetermineLargestWord.findTheLargestWord(s));
+        Assert.assertEquals(false, Anagram.isAnagram("CAT","ARMY"));
     }
 }
