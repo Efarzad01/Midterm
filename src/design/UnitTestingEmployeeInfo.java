9e6c053e87ece3538679da5914e0ad04f4a2d71b
Efarzad01 <emdadullahfarzad@gmail.com>
2018-09-11 17:49:07 -0400

    employee unit T

--- src/design/UnitTestingEmployeeInfo.java
+++ src/design/UnitTestingEmployeeInfo.java
@@ -1,8 +1,15 @@
 package design;
 
+import org.testng.Assert;
+
 public class UnitTestingEmployeeInfo {
     public static void main(String[] args) {
         //Write Unit Test for all the methods has been implemented in this package.
-
+        EmployeeInfo empl00 = new EmployeeInfo();
+        empl00.getemp("Saad");
+        empl00.calculateSalary();
+        Assert.assertEquals(10,empl00.deptId);
+        Assert.assertEquals(4,empl00.employeeName());
+        Assert.assertEquals(70,empl00.calculateSalary());
     }
 }
